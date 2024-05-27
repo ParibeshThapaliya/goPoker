56ff0ca6e9a1e8c500b0c8a20b121da47ce04ee7
Paribesh Thapaliya <74086770+ParibeshThapaliya@users.noreply.github.com>
2024-05-27 08:02:09 -0700

    added pictured and changed source directory i hope the images work

--- src/GUI.c
+++ src/GUI.c
@@ -38,8 +38,8 @@ GtkWidget *Middle1;
 GtkWidget *Middle2;
 GtkWidget *Middle3;
 
-const char *RedBox = "./assets/table/redbox.png";
-const char *GreenBox = "./assets/table/greenbox.png";
+const char *RedBox = "../lib/table/redbox.png";
+const char *GreenBox = "../lib/table/greenbox.png";
 
 // Function declarations
 void startbtnclicked(GtkWidget *widget, gpointer data);
@@ -71,43 +71,43 @@ GtkWidget* create_main_window(void) {
     g_signal_connect(startbtn, "clicked", G_CALLBACK(startbtnclicked), NULL);
 
     // Creating table edges
-    TableTop = gtk_image_new_from_file("./assets/table/topleft.png");
+    TableTop = gtk_image_new_from_file("../lib/table/topleft.png");
     gtk_widget_set_size_request((GtkWidget *)TableTop, 650, 50);
     gtk_table_attach(GTK_TABLE(gametable), TableTop,  0, 13, 0, 1, GTK_FILL, GTK_FILL, 0, 0);
-    TableBot = gtk_image_new_from_file("./assets/table/botleft.png");
+    TableBot = gtk_image_new_from_file("../lib/table/botleft.png");
     gtk_widget_set_size_request((GtkWidget *)TableBot, 650, 50);
     gtk_table_attach(GTK_TABLE(gametable), TableBot,  0, 13, 9, 10, GTK_FILL, GTK_FILL, 0, 0);
-    TableLeft = gtk_image_new_from_file("./assets/table/leftside.png");
+    TableLeft = gtk_image_new_from_file("../lib/table/leftside.png");
     gtk_widget_set_size_request((GtkWidget *)TableLeft, 50, 400);
     gtk_table_attach(GTK_TABLE(gametable), TableLeft,  0, 1, 1, 9, GTK_FILL, GTK_FILL, 0, 0);
-    TableRight = gtk_image_new_from_file("./assets/table/rightside.png");
+    TableRight = gtk_image_new_from_file("../lib/table/rightside.png");
     gtk_widget_set_size_request((GtkWidget *)TableRight, 50, 400);
     gtk_table_attach(GTK_TABLE(gametable), TableRight,  12, 13, 1, 9, GTK_FILL, GTK_FILL, 0, 0);
 
     // Inner
-    TopRightFourTwo = gtk_image_new_from_file("./assets/table/2by4square.png");
+    TopRightFourTwo = gtk_image_new_from_file("../lib/table/2by4square.png");
     gtk_widget_set_size_request((GtkWidget *)TopRightFourTwo, 100, 200);
-    BotRightFourTwo = gtk_image_new_from_file("./assets/table/2by4square.png");
+    BotRightFourTwo = gtk_image_new_from_file("../lib/table/2by4square.png");
     gtk_widget_set_size_request((GtkWidget *)BotRightFourTwo, 100, 200);
-    TopLeftFourTwo = gtk_image_new_from_file("./assets/table/2by4square.png");
+    TopLeftFourTwo = gtk_image_new_from_file("../lib/table/2by4square.png");
     gtk_widget_set_size_request((GtkWidget *)TopLeftFourTwo, 100, 200);
-    BotLeftFourTwo = gtk_image_new_from_file("./assets/table/2by4square.png");
+    BotLeftFourTwo = gtk_image_new_from_file("../lib/table/2by4square.png");
     gtk_widget_set_size_request((GtkWidget *)BotLeftFourTwo, 100, 200);
-    TopLeftTwoTwo = gtk_image_new_from_file("./assets/table/2by2square.png");
+    TopLeftTwoTwo = gtk_image_new_from_file("../lib/table/2by2square.png");
     gtk_widget_set_size_request((GtkWidget *)TopLeftTwoTwo, 100, 100);
-    TopRightTwoTwo = gtk_image_new_from_file("./assets/table/2by2square.png");
+    TopRightTwoTwo = gtk_image_new_from_file("../lib/table/2by2square.png");
     gtk_widget_set_size_request((GtkWidget *)TopRightTwoTwo, 100, 100);
-    TopMiddle1 = gtk_image_new_from_file("./assets/table/2by2square.png");
+    TopMiddle1 = gtk_image_new_from_file("../lib/table/2by2square.png");
     gtk_widget_set_size_request((GtkWidget *)TopMiddle1, 100, 100);
-    TopMiddle2 = gtk_image_new_from_file("./assets/table/1by2square.png");
+    TopMiddle2 = gtk_image_new_from_file("../lib/table/1by2square.png");
     gtk_widget_set_size_request((GtkWidget *)TopMiddle2, 50, 100);
-    BotLeftTwoTwo = gtk_image_new_from_file("./assets/table/2by2square.png");
+    BotLeftTwoTwo = gtk_image_new_from_file("../lib/table/2by2square.png");
     gtk_widget_set_size_request((GtkWidget *)BotLeftTwoTwo, 100, 100);
-    BotRightTwoTwo = gtk_image_new_from_file("./assets/table/2by2square.png");
+    BotRightTwoTwo = gtk_image_new_from_file("../lib/table/2by2square.png");
     gtk_widget_set_size_request((GtkWidget *)BotRightTwoTwo, 100, 100);
-    BotMiddle1 = gtk_image_new_from_file("./assets/table/2by2square.png");
+    BotMiddle1 = gtk_image_new_from_file("../lib/table/2by2square.png");
     gtk_widget_set_size_request((GtkWidget *)BotMiddle1, 100, 100);
-    BotMiddle2 = gtk_image_new_from_file("./assets/table/1by2square.png");
+    BotMiddle2 = gtk_image_new_from_file("../lib/table/1by2square.png");
     gtk_widget_set_size_request((GtkWidget *)BotMiddle2, 50, 100);
     gtk_table_attach(GTK_TABLE(gametable), TopRightFourTwo,  10, 12, 1, 5, GTK_FILL, GTK_FILL, 0, 0);
     gtk_table_attach(GTK_TABLE(gametable), TopLeftFourTwo,  1, 3, 1, 5, GTK_FILL, GTK_FILL, 0, 0);
@@ -123,13 +123,13 @@ GtkWidget* create_main_window(void) {
     gtk_table_attach(GTK_TABLE(gametable), BotMiddle2,  7, 8, 7, 9, GTK_FILL, GTK_FILL, 0, 0);
 
     // Middle for cards
-    MiniTop = gtk_image_new_from_file("./assets/table/5by1square.png");
-    MiniBot = gtk_image_new_from_file("./assets/table/5by1square.png");
-    MiniLeft = gtk_image_new_from_file("./assets/table/1by4square.png");
-    MiniRight = gtk_image_new_from_file("./assets/table/1by4square.png");
-    Middle1 = gtk_image_new_from_file("./assets/table/2by2square.png");
-    Middle2 = gtk_image_new_from_file("./assets/table/2by2square.png");
-    Middle3 = gtk_image_new_from_file("./assets/table/1by2square.png");
+    MiniTop = gtk_image_new_from_file("../lib/table/5by1square.png");
+    MiniBot = gtk_image_new_from_file("../lib/table/5by1square.png");
+    MiniLeft = gtk_image_new_from_file("../lib/table/1by4square.png");
+    MiniRight = gtk_image_new_from_file("../lib/table/1by4square.png");
+    Middle1 = gtk_image_new_from_file("../lib/table/2by2square.png");
+    Middle2 = gtk_image_new_from_file("../lib/table/2by2square.png");
+    Middle3 = gtk_image_new_from_file("../lib/table/1by2square.png");
     gtk_widget_set_size_request((GtkWidget *)MiniTop, 250, 50);
     gtk_widget_set_size_request((GtkWidget *)MiniBot, 250, 50);
     gtk_widget_set_size_request((GtkWidget *)MiniLeft, 50, 200);
