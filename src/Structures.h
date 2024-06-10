bea06c02ec0b4c9ec8e5dd7a15b0cdc79b15a99f
Mitzillfi <paribesh.thapaliya@gmail.com>
2024-06-10 09:28:32 -0700

    more typos

--- src/Structures.h
+++ src/Structures.h
@@ -1,24 +1,65 @@
-//This file has the headers for the caard and player structuees
+// This file has the headers for the caard and player structuees
 
-//Data structures
+// Data structures
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
 
+typedef enum
+{
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+} RANK;
+typedef enum
+{
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+} SUIT;
+typedef enum
+{
+    NONE,
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush,
+    RoyalFlush
+} HAND;
+typedef enum
+{
+    CHECK,
+    RAISE,
+    FOLD,
+    CALL
+} ACTION;
 
-typedef enum {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE} RANK;
-typedef enum {CLUBS, DIAMONDS, HEARTS, SPADES} SUIT;
-typedef enum {NONE, HighCard, OnePair, TwoPair, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraightFlush, RoyalFlush} HAND;
-typedef enum {CHECK, RAISE, FOLD, CALL} ACTION;
-
-typedef struct {
+typedef struct
+{
     SUIT suit;
     RANK rank;
 } Card;
 
 Card Deck[52];
 Card boardCards[5];
-
-typedef struct {
+int pot;
+typedef struct
+{
     char name[50];
     int points;
     int seat;
@@ -31,19 +72,19 @@ typedef struct {
 
 Player PlayersList[10];
 
-Player* createPlayer(const char* name, int points, int seat, int socket);
+Player *createPlayer(const char *name, int points, int seat, int socket);
 
-void deletePlayer(Player* player);
+void deletePlayer(Player *player);
 
-//functions
-Card* createCard(SUIT suit, RANK rank);
+// functions
+Card *createCard(SUIT suit, RANK rank);
 
-void deleteCard(Card* card);
+void deleteCard(Card *card);
 
 void initializeDeck(Card deck[52]);
 
-void deletePlayer(Player* player);
+void deletePlayer(Player *player);
 
 void initializePlayers(Player playersList[], int numPlayers);
 
-#endif  
+#endif
