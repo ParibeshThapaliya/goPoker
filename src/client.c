696290bd97e40189ca425a39c4db49cc1c079234
Mitzillfi <paribesh.thapaliya@gmail.com>
2024-06-10 17:01:16 -0700

    semi colon fix

--- src/client.c
+++ src/client.c
@@ -31,8 +31,8 @@ int main(int argc, char *argv[])
 
     GtkWidget *window;
     GtkWidget *button;
-    GtkWidget *BG
-        gtk_init(&argc, &argv);
+    GtkWidget *BG;
+    gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
     g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
